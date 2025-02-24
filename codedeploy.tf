8b23078c9ee3ec399ac9406e1011aa2ec01edd11
Muralee2 <muralimvkaws1@gmail.com>
2025-02-24 13:39:56 +0530

    Update codedeploy.tf
    ff

--- codedeploy.tf
+++ codedeploy.tf
@@ -3,19 +3,24 @@ resource "aws_codedeploy_app" "webapp" {
   name             = "webapp"
 }
 
-resource "aws_codedeploy_deployment_group" "CodeDeployWebapp" {
-  app_name              = aws_codedeploy_app.webapp.name
-  deployment_group_name = "CodeDeployWebapp"
-  service_role_arn      = aws_iam_role.codedeploy_role.arn
-}
-  autoscaling_groups  = [aws_autoscaling_group.webapp_asg.name]
+resource "aws_codedeploy_deployment_group" "example" {
+  app_name              = aws_codedeploy_app.example.name
+  deployment_group_name = "example-group"
+  service_role_arn      = aws_iam_role.codedeploy.arn
+
+  autoscaling_groups = [aws_autoscaling_group.webapp_asg.name] # ⚠️ Check if this is valid in your case
+
+  deployment_config_name = "CodeDeployDefault.OneAtATime"
 
   load_balancer_info {
     target_group_info {
-      name = aws_lb_target_group.webapp-TG.name
+      name = aws_lb_target_group.example.name
     }
   }
-    deployment_style {
-    deployment_type = "IN_PLACE"
+
+  deployment_style {
     deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
   }
+}
+
