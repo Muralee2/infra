58b5fbb7c59ea7be2a45e00424792eb8e94d8563
Muralee2 <muralimvkaws1@gmail.com>
2025-02-25 08:41:30 +0530

    Update sonarqube.tf
    sonar

--- sonarqube.tf
+++ sonarqube.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "sonarqube_vm" {
   subnet_id              = aws_subnet.subnet-public-1.id
   user_data              = filebase64("user-data-sonar.sh")
   vpc_security_group_ids = [aws_security_group.sonarQube-SG.id]
-  key_name                = "webapp_keypair"
+  key_name               = "webapp_keypair"
 
   tags = {
     Name = "SonarQube VM"
@@ -20,6 +20,7 @@ resource "aws_security_group" "sonarQube-SG" {
     Name = "sonarQube-SG"
   }
 }
+
 resource "aws_security_group_rule" "allow_port_9000_to_sonar_VM" {
   type              = "ingress"
   from_port         = 9000
@@ -28,11 +29,22 @@ resource "aws_security_group_rule" "allow_port_9000_to_sonar_VM" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sonarQube-SG.id
 }
+
 resource "aws_security_group_rule" "outbound_allow_all_sonar" {
   type              = "egress"
+  from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.sonarQube-SG.id
-  from_port         = 0
+}
+
+# Wait for SonarQube to become available
+resource "null_resource" "wait_for_sonarqube" {
+  provisioner "local-exec" {
+    command = "until $(curl --output /dev/null --silent --head --fail http://${aws_instance.sonarqube_vm.public_ip}:9000); do echo 'Waiting for SonarQube...'; sleep 10; done"
+  }
+
+  depends_on = [aws_instance.sonarqube_vm]
+}
 }
