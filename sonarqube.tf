8d888860cf0411dd5e29bb50aeb5458e7541e86c
Muralee2 <muralimvkaws1@gmail.com>
2025-02-25 11:48:55 +0530

    Update sonarqube.tf
    tfbest

--- sonarqube.tf
+++ sonarqube.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "sonarqube_vm" {
   subnet_id              = aws_subnet.subnet-public-1.id
   user_data              = filebase64("user-data-sonar.sh")
   vpc_security_group_ids = [aws_security_group.sonarQube-SG.id]
-  key_name               = "webapp_keypair"
+  key_name               = "tfbest"  # Replace with your AWS key pair name
 
   tags = {
     Name = "SonarQube VM"
@@ -43,22 +43,5 @@ resource "aws_security_group_rule" "outbound_allow_all_sonar" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.sonarQube-SG.id
-}
+ 
 
-# Wait for SonarQube to become available with timeout
-resource "null_resource" "wait_for_sonarqube" {
-  provisioner "local-exec" {
-    command = <<EOT
-    timeout 600 bash -c 'while ! curl --output /dev/null --silent --head --fail http://${aws_instance.sonarqube_vm.public_ip}:9000; do 
-      echo "Waiting for SonarQube...";
-      sleep 10;
-    done'
-    echo "SonarQube is now reachable!"
-    EOT
-  }
-
-  depends_on = [aws_instance.sonarqube_vm]
-}
