8da5c59508fbb87a62884de46595a75a92600b2a
Muralee2 <muralimvkaws1@gmail.com>
2025-07-16 17:37:21 +0530

    Update s3_policy.tf
    wed

--- s3_policy.tf
+++ s3_policy.tf
@@ -1,16 +1,22 @@
 resource "aws_s3_bucket_policy" "manual_bucket_policy" {
-  bucket = "my-tf-saniakurup-bucket"
+  bucket = "my-tf-saniakurup-bucket"  # Replace with your actual bucket name if needed
 
   policy = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "AllowPublicRead",
       "Effect": "Allow",
       "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::my-tf-saniakurup-bucket/*"
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "arn:aws:s3:::my-tf-saniakurup-bucket",
+        "arn:aws:s3:::my-tf-saniakurup-bucket/*"
+      ]
     }
   ]
 }
