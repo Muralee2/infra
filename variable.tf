a54fee617062d4426903d4f2585f073ca1b1c216
Muralee2 <muralimvkaws1@gmail.com>
2025-07-19 15:20:10 +0530

    Update variable.tf
    ee

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 
-variable "static_website_bucket_name" {
-  type    = string
-  default = "my-tf-saniakurup-bucket"
-}
+#variable "static_website_bucket_name" {
+#  type    = string
+#  default = "my-tf-saniakurup-bucket"
+#}
 variable "build-bucker-name" {
   type    = string
   default = "hypha-saniakurup-bucket"
